7fc9d930336649201540e3846135b4f6d79b4b8f
Gisele Macieira <giselefchaves@gmail.com>
2023-04-06 21:38:13 -0300

    versão com o flip das cartas

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ while (qtdDeCartas<4 || qtdDeCartas>14 || qtdDeCartas%2 !== 0){
 }
 while (contador <= qtdDeCartas){
     let elemento = document.querySelector(".conteudo");
-    elemento.innerHTML = elemento.innerHTML + "<div class='carta-verso'><img class='img-verso' src='./Imagens/back.png'/></div>"
+    elemento.innerHTML = elemento.innerHTML + "<div onclick= 'flip (this)' class='carta'><div class='frente face'><img class='img-verso' src='./Imagens/back.png'/></div><div class='verso face'><img class='img-verso' src='./Imagens/bobrossparrot.gif'/></div></div>"
     contador = contador + 1;
+}
+function flip (cartaSelecionada){
+    const carta = cartaSelecionada.querySelectorAll("div")
+    const frente = carta[0];
+    const verso = carta[1];
+    frente.classList.toggle("frente-ativa");
+    verso.classList.toggle("verso-ativa");
+    console.log(face);
 }
